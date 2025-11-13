38bee1d6d96b32a0ff4bacc4acf43636f4f0a6bd
Nipun Wijethunga <nmwijethunga@students.nsbm.ac.lk>
2025-11-13 23:55:37 +0530

    Project structure update

---
+++ bankapp-backend/src/main/java/com/bankapp/bankapp_backend/BankappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bankapp.bankapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappBackendApplication.class, args);
+	}
+
+}
